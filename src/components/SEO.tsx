622ef80ae68d7a8f48d4d35fe2d819da6749e652
Cengiz YILMAZ <62932614+cengizyilmaz1@users.noreply.github.com>
2025-06-04 16:55:40 +0300

    seo

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -596,14 +596,7 @@ const SEO: React.FC<SEOProps> = ({
         rel="preload" 
         href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" 
         as="style" 
-        onLoad={() => {}} 
       />
-      <noscript>
-        <link 
-          rel="stylesheet" 
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" 
-        />
-      </noscript>
       
       {/* Favicon and Icons */}
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
